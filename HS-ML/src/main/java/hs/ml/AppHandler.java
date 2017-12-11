af54add1e97c036229c1920b73527eacdefb2383
Windows-7 <Windows-7@FViale>
2017-12-11 19:19:58 -0300

    Se agrego las ventanas para abrir la url de autenticacion

--- HS-ML/src/main/java/hs/ml/AppHandler.java
+++ HS-ML/src/main/java/hs/ml/AppHandler.java
@@ -7,36 +7,45 @@ package hs.ml;
 
 import Meli.AuthorizationFailure;
 import Meli.Meli;
-import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
+import com.panamahitek.webbrowserexample.JFrameWindow;
 
 /**
  *
  * @author Windows-7
  */
 public class AppHandler {
-    String auth_Url;
-    String[] parts_Url;
+    Meli meli_App; 
     String code_app;
     
-    public AppHandler(long app_id, String app_pass, String api_url) throws AuthorizationFailure, URISyntaxException, IOException{
-        Meli meli_App = new Meli(app_id, app_pass);
-        auth_Url = meli_App.getAuthUrl(api_url, Meli.AuthUrls.MLA);
+    public AppHandler(long app_id, String app_pass, String api_url){
+        try{
+            meli_App = new Meli(app_id, app_pass);
+            String auth_Url = meli_App.getAuthUrl(api_url, Meli.AuthUrls.MLA);
         
-        URI uri = new URI(auth_Url);
-        java.awt.Desktop.getDesktop().browse(uri);
-        
-        parts_Url = auth_Url.split("code=");
-        code_app = parts_Url[1];
+            //URI uri = new URI(auth_Url);
+            //java.awt.Desktop.getDesktop().browse(uri);
+            JFrameWindow window = new JFrameWindow(auth_Url);
+            window.setVisible(true);
 
-        meli_App.authorize(code_app, api_url);
-        System.out.println("user_id = " + meli_App.getUserId() +
-                           "tokenType = " + meli_App.getTokenType() +
-                           "accessToken = " + meli_App.getAccessToken() + 
-                           "refreshToken = " + meli_App.getRefreshToken() + 
-                           "expiresIn = " + meli_App.getExpiresIn().toString() + 
-                           "scope = " + meli_App.getScope()
-                          );
+            code_app = "TG-5a2e7233e4b08147b98a3f1f-288530762";
+
+            meli_App.authorize(code_app, api_url);
+            //System.out.println("user_id = " + meli_App.getUserId() +
+            //                   " / tokenType = " + meli_App.getTokenType() +
+            //                   " / accessToken = " + meli_App.getAccessToken() + 
+            //                   " / refreshToken = " + meli_App.getRefreshToken() + 
+            //                   " / expiresIn = " + meli_App.getExpiresIn().toString() + 
+            //                   " / scope = " + meli_App.getScope()
+            //              );           
+       }catch (AuthorizationFailure e){
+       }
+    }
+    
+    public String getAccessToken(){
+        return meli_App.getAccessToken();
+    }
+      
+    public Long getExpiresIn(){
+       return meli_App.getExpiresIn(); 
     }
 }
